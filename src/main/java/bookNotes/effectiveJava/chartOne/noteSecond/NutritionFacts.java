76dfe5d9c0451edfcfa332b542840ee576d2f962
Shelton_Lee <mr.lixiaosheng@gmail.com>
2019-04-01 23:34:45 +0800

    使用构建器，编写一个类。

---
+++ src/main/java/bookNotes/effectiveJava/chartOne/noteSecond/NutritionFacts.java
@@ -0,0 +1,61 @@
+package bookNotes.effectiveJava.chartOne.noteSecond;
+
+public class NutritionFacts {
+    private final int serviceSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder{
+        private final int serviceSize;
+        private final int servings;
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
+
+        public Builder(int serviceSize, int servings){
+            this.serviceSize = serviceSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int val){
+            calories = val;
+            return this;
+        }
+
+        public Builder fat(int val){
+            fat = val;
+            return this;
+        }
+
+        public Builder sodium(int val){
+            sodium = val;
+            return this;
+        }
+
+        public Builder carbohydrate(int val){
+            carbohydrate = val;
+            return this;
+        }
+
+        public NutritionFacts build(){
+            return new NutritionFacts(this);
+        }
+    }
+
+    public NutritionFacts(Builder builder){
+        serviceSize = builder.serviceSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+
+    public static void main(String[] args) {
+        NutritionFacts nutritionFacts = new NutritionFacts.Builder(240, 8).calories(100).sodium(20).carbohydrate(35).build();
+    }
+}
